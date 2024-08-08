a5e976813bde6b73b6857966c774d56ae98e0287
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-08-07 21:52:09 -0300

    Criando Rota PUT com parâmetro no Express para atualizar dados

--- NodeApi/src/app.js
+++ NodeApi/src/app.js
@@ -22,8 +22,14 @@ function buscarSelecaoId(id){
 
 //Localizar posição/index do elemento
 function buscarIndexSelecao(id){
-  return selecoes.findIndex(selecao=> selecao.id==id)
+  return selecoes.findIndex(selecao=> selecao.id ==id)
 }
+
+//Rota para Criar dados usando post
+app.post('/selecoes',(req,res)=>{
+  selecoes.push(req.body)
+  res.status(201).send('seleção cadastrada com sucesso')
+})
 
 //Rota raiz
 app.get('/',(req,res)=>{
@@ -40,19 +46,25 @@ app.get('/selecoes/:id', (req,res)=>{
     res.json(buscarSelecaoId(req.params.id))
 })
 
-//Rota para postar dados usando post
+//Fazer atualizações por id usando put
+app.put('/selecoes/:id', (req,res)=>{
+  let index= buscarIndexSelecao(req.params.id)
+  selecoes[index].selecao = req.body.selecao 
+  selecoes[index].grupo   = req.body.grupo 
+  res.json(selecoes)
 
-app.post('/selecoes',(req,res)=>{
-  selecoes.push(req.body)
-  res.status(201).send('seleção cadastrada com sucesso')
+  //Obs: o id pode ser alterado porem, não é recomendável, para garantirmos a integridade.
 })
 
+//deletar por id
 app.delete('/selecoes/:id', (req,res)=>{
   let index= buscarIndexSelecao(req.params.id)
   selecoes.splice(index, 1)
   //.Splice Método usado para remover elemento do array
   res.send(`Seleção com id ${req.params.id} excluida com sucesso!`)
 })
+
+
 
 export default app
 
