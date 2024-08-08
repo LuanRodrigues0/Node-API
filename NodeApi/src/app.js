a88b55719b544b992f28c88202d31e51bb075a8a
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-08-07 21:19:48 -0300

    .....
    Aprendi a criar Rotas com parâmetro no Express e criar rota DELETE no Express

--- NodeApi/src/app.js
+++ NodeApi/src/app.js
@@ -2,14 +2,28 @@
 import express from 'express'
 const app = express()
 
+
+
+//Indicar para o express ler o body com json
+app.use(express.json())
+
 //mock
 const selecoes=[
   {id:1, selecao: 'Brasil', grupo:'G'},
   {id:2, selecao: 'Suiça', grupo:'G'},
-  {id:3, selecao: 'Sérvia', grupo:'G'},
-  {id:4, selecao: 'Camarões', grupo:'G'},
+  {id:3, selecao: 'Camarões', grupo:'G'},
+  {id:4, selecao: 'Sérvia', grupo:'G'},
 ]
 
+//Buscar por id
+function buscarSelecaoId(id){
+  return selecoes.filter(selecao=> selecao.id ==id)
+}
+
+//Localizar posição/index do elemento
+function buscarIndexSelecao(id){
+  return selecoes.findIndex(selecao=> selecao.id==id)
+}
 
 //Rota raiz
 app.get('/',(req,res)=>{
@@ -21,11 +35,23 @@ app.get('/Selecoes', (request,response)=>{
 
 })
 
+//Buscar por id
+app.get('/selecoes/:id', (req,res)=>{
+    res.json(buscarSelecaoId(req.params.id))
+})
+
 //Rota para postar dados usando post
 
 app.post('/selecoes',(req,res)=>{
   selecoes.push(req.body)
   res.status(201).send('seleção cadastrada com sucesso')
+})
+
+app.delete('/selecoes/:id', (req,res)=>{
+  let index= buscarIndexSelecao(req.params.id)
+  selecoes.splice(index, 1)
+  //.Splice Método usado para remover elemento do array
+  res.send(`Seleção com id ${req.params.id} excluida com sucesso!`)
 })
 
 export default app
