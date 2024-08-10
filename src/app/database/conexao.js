75bcc1351ad5e3ef5a3f83962ce3c73354c47295
LuanRodrigues0 <164959288+LuanRodrigues0@users.noreply.github.com>
2024-08-10 20:51:39 -0300

    ....

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -11,6 +11,25 @@ const conexao = mysql.createConnection({
 
 conexao.connect()
 
+
+/**
+ * Executa um código sql com ou sem valores
+ * @param {string} sql instrução sql a ser executada
+ * @param {string=id | [selecao, id]} valores valores a serem passados ao sql 
+ * @param {string} mensagemReject mensagem a ser exibida
+ * @returns objeto da promisse
+ */
+ export const consulta = (sql, valores='', mensagemReject) =>{
+   return new Promise((resolve,reject)=>{
+      conexao.query(sql, valores, (erro,result)=>{
+          if(erro) return reject(mensagemReject)
+            
+          const row = JSON.parse(JSON.stringify(result))
+          return resolve(row)
+      })
+  })
+}
+
 export default conexao
 
 //Caso erro "Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; ",  siga os seguintes passos no terminal:
--- src/app/repositories/SelecaoRepository.js
+++ src/app/repositories/SelecaoRepository.js
@@ -1,71 +1,28 @@
-import { json } from "express"
-import conexao from "../database/conexao.js"
+import {consulta} from "../database/conexao.js"
 class selecaoRepository{
     //CRUD
     create(selecao)  {
         const sql= "INSERT INTO selecoes SET ? "
-        return new Promise((resolve,reject)=>{
-            conexao.query(sql,selecao, (erro,result)=>{
-                if(erro) return reject('Não foi possível cadastrar')
-                
-                 //Fazer o parse dos resultados   
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row)
-            })
-        })
+        return consulta(sql, selecao, 'Não foi possível criar!')
     }
 
     findAll() {
         const sql= "SELECT * FROM selecoes;"
-        return new Promise((resolve,reject)=>{
-            conexao.query(sql,(erro,result)=>{
-                if(erro) return reject('Não foi possível localizar')
-                
-                 //Fazer o parse dos resultados   
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row)
-            })
-        })
+        return consulta(sql, 'seleção não encontrada!')
     }
     findById(id){
         const sql= "SELECT * FROM selecoes WHERE id=?;"
-        return new Promise((resolve,reject)=>{
-            conexao.query(sql,id, (erro,result)=>{
-               if(erro) return reject('Não foi possível localizar o id')
-                
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row)
-         
-            })  
-
-        })  
+        return consulta(sql, id, 'ID não encontrado!')
     }
 
     update(selecao, id) {
         const sql = "UPDATE selecoes SET ? WHERE id=?; "
-        return new Promise((resolve,reject)=>{
-            conexao.query(sql,[selecao,id],(erro,result)=>{
-                if(erro) return reject('Não foi possível atualizar a seleção')
-                
-                
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row) 
-            })
-        }) 
+        return consulta(sql, [selecao, id], 'não foi possível atualizar!')
     }
 
     delete(id){
         const sql= "DELETE FROM selecoes WHERE id=?;"
-        return new Promise((resolve,reject)=>{
-            conexao.query(sql,id,(erro,result)=>{
-                if(erro) return reject("Não foi possível apagar")
-
-
-                const row = JSON.parse(JSON.stringify(result))
-                return resolve(row)
-
-            })
-        })
+        return consulta(sql, id, 'Não foi possível apagar!')
     }
 
 }
